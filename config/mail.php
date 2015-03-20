f6d34018973519e4097c4e5ecaa6c979f60df788
Ro Kleine Sonne <ro@greengoenergy.dk>
2015-03-20 08:39:28 +0100

    added sender mail address

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'it@greengoenergy.dk', 'name' => 'GreenGo Energy A/S'],
 
 	/*
 	|--------------------------------------------------------------------------
