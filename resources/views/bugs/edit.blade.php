b8cb5b7e8073cae2dde27f5c3f3c96b8c20677a6
Ro Kleine Sonne <ro.sonne@gmail.com>
2017-03-03 10:53:14 +0100

    fixed create new bug bug

--- resources/views/bugs/edit.blade.php
+++ resources/views/bugs/edit.blade.php
@@ -11,6 +11,18 @@
             <div class="form">
               {!! Form::model($bug, ['method' => 'PATCH', 'action' => ['BugsController@update', $bug->id], 'class' => 'cmxform form-horizontal']) !!}
 
+                @if(Auth::user()->id == $bug->assigned_to)
+
+                <div class="form-group @if ($errors->has('status')) has-error @endif">
+                    {!! Form::label('status', 'Status:', array('class' => 'control-label col-lg-3')) !!}
+                    <div class="col-lg-6">
+                      {!! Form::select('status', [null => 'Please Select'] + Greengo\Models\BugStatus::all()->pluck('title', 'id')->toArray(), null, array('class' => 'form-control')) !!}
+                      {{ ($errors->has('status') ?  $errors->first('status') : '') }}
+                    </div>
+                </div>
+
+                @endif
+                
                 @include('bugs._form', ['submitText' => 'Update Bug'])
 
             {!! Form::close() !!}
